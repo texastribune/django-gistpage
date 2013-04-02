928cb47ba2533c5cb76bc8a7e9bc81c0b6d28bc7
crccheck <c@crccheck.com>
2013-04-02 16:54:00 +0000

    populate gistpage attrs from the github gist

--- gistpage/admin.py
+++ gistpage/admin.py
@@ -1,4 +1,8 @@
+import json
+
 from django.contrib import admin
+import requests
+
 from .models import GistPage
 
 
@@ -6,7 +10,43 @@ class GistPageAdmin(admin.ModelAdmin):
     fields = (
         'gist_url',
         'site_url',
+        'template',
+        'style',
+        'script',
         # 'sites',
     )
+
+    def save_model(self, request, obj, form, change):
+        def process(gist_files_dict):
+            for filename, meta in gist_files_dict.items():
+                yield meta['filename'], meta['content']
+
+        # http://developer.github.com/v3/gists/#get-a-single-gist
+        gist_id = obj.gist_url.strip('/').split('/')[-1]
+        endpoint = 'https://api.github.com/gists/'
+        response = requests.get('%s%s' % (endpoint, gist_id))
+        if response.status_code != 200:
+            # TODO validation error
+            return
+
+        # clear out old values
+        obj.template = ''
+        obj.style = ''
+        obj.script = ''
+
+        data = json.loads(response.content)
+        files = data['files']
+
+        for filename, content in process(files):
+            if filename == 'index.html':
+                obj.template = content
+            extension = filename.rsplit('.', 1)[-1]
+            if extension == 'css':
+                obj.style += content
+            if extension == 'js':
+                obj.script += content
+
+        obj.save()
+
 
 admin.site.register(GistPage, GistPageAdmin)
--- gistpage/models.py
+++ gistpage/models.py
@@ -1,4 +1,4 @@
-from django.conf import settings
+# from django.conf import settings
 from django.contrib.sites.models import Site
 from django.db import models
 
@@ -7,12 +7,12 @@ class GistPage(models.Model):
     """Similar to a Flatpage, but backed by a Github.com gist."""
     #
     gist_url = models.URLField()
-    template = models.TextField()
-    style = models.TextField()
-    script = models.TextField()
+    template = models.TextField(null=True, blank=True)
+    style = models.TextField(null=True, blank=True)
+    script = models.TextField(null=True, blank=True)
 
     # integration fields
-    site_url = models.CharField(max_length=200,
+    site_url = models.CharField(max_length=200, null=True, blank=True,
         help_text="The url for this page on the site. e.g.: /test/page/")
     sites = models.ManyToManyField(Site,
         # default=settings.SITE_ID
