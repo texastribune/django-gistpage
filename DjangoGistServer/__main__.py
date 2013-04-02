c759c094484acedb48a3941a8ea9a1015fb692a4
crccheck <c@crccheck.com>
2013-04-02 17:56:09 +0000

    convert server to use optparse

--- DjangoGistServer/__main__.py
+++ DjangoGistServer/__main__.py
@@ -1,11 +1,19 @@
+from optparse import OptionParser
+
 import os
 import sys
 
 
+DEFAULT_PORT = 8000
+
 if __name__ == "__main__":
-    port = 8000
-    if len(sys.argv) > 1:
-        port = sys.argv[-1]
+    usage = "usage: python -m DjangoGistServer [options] [port]"
+    parser = OptionParser(usage=usage)
+    # parser.add_option()
+    (options, args) = parser.parse_args()
+    port = DEFAULT_PORT
+    if args:
+        port = args[0]
     sys.path.append(os.path.abspath('.'))
     os.environ["DJANGO_SETTINGS_MODULE"] = "gistserver_project.settings"
     from django.core.management import call_command
