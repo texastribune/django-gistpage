ae9dd62af20a3cbc28b71df5ae7497ffde22fa4d
crccheck <c@crccheck.com>
2013-04-02 15:38:53 +0000

    do the same, serve all css too

--- gistpage/urls.py
+++ gistpage/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = patterns('',
     url(r'^$', views.Page.as_view()),
+    url(r'^app.css$', views.Glob.as_view(pattern="*.css")),
     url(r'^app.js$', views.Glob.as_view(pattern="*.js")),
 )
