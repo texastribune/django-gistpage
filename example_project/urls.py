644c11a61016cc2d0f47c3b8d4c25b268279f3c8
crccheck <c@crccheck.com>
2013-04-02 13:35:50 +0000

    initial commit

---
+++ example_project/urls.py
@@ -0,0 +1,40 @@
+from django.conf import settings
+from django.conf.urls import patterns, include, url
+from django.views.static import serve
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^', include('gistpage.urls',
+        namespace='', app_name='')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+
+    # Use Django to serve static media even when DEBUG=False
+    url(r'^static/(?P<path>.*)$', serve, {
+        'document_root': settings.STATIC_ROOT,
+    }),
+)
+
+
+# extra urlpatterns for development used for serving media and for serving a
+# dummy favicon.
+if settings.DEBUG:
+    from django.http import HttpResponse
+
+    def favicon(request):
+        image_data = open("example_project/static/favicon.ico", "rb").read()
+        return HttpResponse(image_data, mimetype="image/x-icon")
+
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+        url(r'^favicon.ico$', favicon),
+    )
---
+++ setup.py
@@ -0,0 +1,22 @@
+exit("NOPE!")
+
+from setuptools import setup
+
+setup(
+    name='',
+    version='0.1.0',
+    author='',
+    author_email='',
+    url='',
+    packages=[''],
+    include_package_data=True,  # automatically include things from MANIFEST.in
+    license='Apache License, Version 2.0',
+    description='',
+    long_description=open('README.rst').read(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+)
